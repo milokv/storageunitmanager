f622b3208e594baca2aac8523a6aa28901b1f62e
Milo <milo.kvarnberg@gmail.com>
2024-11-12 20:54:28 +0100

    added local .env file to store pass, temporary measure for testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const SteamUser = require('steam-user');
 const GlobalOffensive = require('globaloffensive');
+require('dotenv').config();
 
 let user = new SteamUser();
 let csgo = new GlobalOffensive(user);
